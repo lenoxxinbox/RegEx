83306078e483f596e9a7400b3890d5062db7421f
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2021-11-27 13:05:58 +0300

    Update hw_reg_ex.py
    list

--- hw_reg_ex.py
+++ hw_reg_ex.py
@@ -2,19 +2,19 @@ import re
 import csv
 
 with open("phonebook_raw.csv") as f:
-    rows = csv.DictReader(f, delimiter=",")
+    rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
 
 fio_list = []
 for cl in contacts_list:
-    phone_number = cl['phone']
+    phone_number = cl[-2]
     phone = re.compile(r"((\+)?7|8)?\s*\(*495-* *\)* *(\d\d\d)-*(\d\d)-*(\d\d) *\(*(доб. )*(\d*)\)*")
     tel = phone.sub(r"+7(495)\3-\4-\5 \6\7", phone_number)
-    fio = (cl['lastname'] + " " + cl['firstname'] + " " + cl['surname'] + " ").split()
-    fio.append(cl['organization'])
-    fio.append(cl['position'])
+    fio = (cl[0] + " " + cl[1] + " " + cl[2] + " ").split()
+    fio.append(cl[3])
+    fio.append(cl[4])
     fio.append(tel)
-    fio.append(cl['email'])
+    fio.append(cl[6])
     fio_list.append(fio)
 uniqueFio = []
 for item in fio_list:
